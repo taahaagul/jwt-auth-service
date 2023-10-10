68de5cf54ebb0181be052c2b481ede7a003d996c
TaahaaGul <tahagulll06@gmail.com>
2023-10-10 08:42:38 +0300

    init commit

---
+++ src/main/java/com/taahaagul/tgjwtsecurity/TgJwtSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.taahaagul.tgjwtsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgJwtSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgJwtSecurityApplication.class, args);
+	}
+
+}
