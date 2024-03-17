6836dcd47bd8b90b28995409dd759452c25848f1
taahaagul <tahagulll06@gmail.com>
2024-03-17 10:10:54 +0300

    updated Authentication Service ./

--- src/main/java/com/taahaagul/jwtauthservice/service/AuthenticationService.java
+++ src/main/java/com/taahaagul/jwtauthservice/service/AuthenticationService.java
@@ -22,7 +22,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.time.LocalDate;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Optional;
@@ -34,7 +33,6 @@ import java.util.UUID;
 public class AuthenticationService {
 
     private final UserRepository userRepository;
-    private final TokenRepository tokenRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
     private final AuthenticationManager authenticationManager;
@@ -42,14 +40,6 @@ public class AuthenticationService {
     private final MailService mailService;
 
     public void register(RegisterRequest request) {
-        Optional<User> existingUserName = userRepository.findByUserName(request.getUserName());
-        if(existingUserName.isPresent())
-            throw new UserNotFoundException("Username already is exist");
-
-        Optional<User> existingUser = userRepository.findByEmail(request.getEmail());
-        if (existingUser.isPresent()) {
-            throw new UserNotFoundException("Email already exist!");
-        }
 
         var user = User.builder()
                 .firstName(request.getFirstName())
@@ -57,8 +47,7 @@ public class AuthenticationService {
                 .userName(request.getUserName())
                 .email(request.getEmail())
                 .password(passwordEncoder.encode(request.getPassword()))
-                .memberSince(LocalDate.now())
-                .role(Role.USER)
+                .role(Role.valueOf("USER"))
                 .enabled(false)
                 .build();
         userRepository.save(user);
@@ -68,47 +57,6 @@ public class AuthenticationService {
                 user.getEmail(), "Thank you for signing up to Spring Reddit, " +
                 "please click on the below url to activate your account : " +
                 "http://localhost:8080/api/auth/accountVerification/" + token));
-    }
-
-    public String generateVerificationToken(User user) {
-        VerificationToken existingToken = verificationTokenRepository.findByUser(user);
-
-        if(existingToken != null) {
-            verificationTokenRepository.delete(existingToken);
-            log.info("Deleted existing verification token");
-        } else {
-            log.info("There is no any verification token");
-        }
-
-        String token = UUID.randomUUID().toString();
-
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(new Date());
-        calendar.add(Calendar.MINUTE, 4);
-        Date expirationTime = calendar.getTime();
-
-        VerificationToken verificationToken = VerificationToken.builder()
-                .token(token)
-                .user(user)
-                .expirationTime(expirationTime)
-                .build();
-
-        verificationTokenRepository.save(verificationToken);
-        return token;
-    }
-
-    public void verifyAccount(String token) {
-        Optional<VerificationToken> verificationToken = verificationTokenRepository.findByToken(token);
-        fetchUserAndEnable(verificationToken.orElseThrow(() -> new UserNotFoundException("Invalid Token")));
-    }
-
-    private void fetchUserAndEnable(VerificationToken verificationToken) {
-        String username = verificationToken.getUser().getUsername();
-        User  user = userRepository.findByEmail(username)
-                .orElseThrow(()-> new UserNotFoundException("User not found with name -" + username));
-        user.setEnabled(true);
-        verificationTokenRepository.delete(verificationToken);
-        userRepository.save(user);
     }
 
     public AuthenticationResponse authenticate(LoginRequest request) {
@@ -122,40 +70,10 @@ public class AuthenticationService {
                 .orElseThrow();
         var jwtToken = jwtService.generateToken(user);
         var refreshToken = jwtService.generateRefreshToken(user);
-        revokeAllUserTokens(user);
-        saveUserToken(user, jwtToken);
         return AuthenticationResponse.builder()
                 .accessToken(jwtToken)
                 .refreshToken(refreshToken)
                 .build();
-    }
-
-    private void revokeAllUserTokens(User user) {
-        /*
-        var validUserTokens = tokenRepository.findAllValidTokenByUser(user.getId());
-        if (validUserTokens.isEmpty())
-            return;
-        validUserTokens.forEach(token -> {
-            token.setExpired(true);
-            token.setRevoked(true);
-        });
-        tokenRepository.saveAll(validUserTokens);
-         */
-        var validUserTokens = tokenRepository.findAllValidTokenByUser(user.getId());
-        if (validUserTokens.isEmpty())
-            return;
-        tokenRepository.deleteAll(validUserTokens);
-    }
-
-    private void saveUserToken(User user, String jwtToken) {
-        var token = Token.builder()
-                .user(user)
-                .token(jwtToken)
-                .tokenType(TokenType.BEARER)
-                .expired(false)
-                .revoked(false)
-                .build();
-        tokenRepository.save(token);
     }
 
     public void refreshToken(
@@ -175,8 +93,6 @@ public class AuthenticationService {
                     .orElseThrow();
             if (jwtService.isTokenValid(refreshToken, user)) {
                 var accessToken = jwtService.generateToken(user);
-                revokeAllUserTokens(user);
-                saveUserToken(user, accessToken);
                 var authResponse = AuthenticationResponse.builder()
                         .accessToken(accessToken)
                         .refreshToken(refreshToken)
@@ -186,10 +102,46 @@ public class AuthenticationService {
         }
     }
 
-    public User getCurrentUser() {
-        User user = userRepository.findByEmail(SecurityContextHolder.getContext().getAuthentication().getName())
-                .orElseThrow(() -> new UserNotFoundException("Current user is not found"));
-        return user;
+    public String generateVerificationToken(User user) {
+        if(user.isEnabled())
+            revokeAllVerificationToken(user);
+
+        String token = UUID.randomUUID().toString();
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+        calendar.add(Calendar.MINUTE, 4);
+        Date expirationTime = calendar.getTime();
+
+        VerificationToken verificationToken = VerificationToken.builder()
+                .token(token)
+                .user(user)
+                .expirationTime(expirationTime)
+                .build();
+
+        verificationTokenRepository.save(verificationToken);
+        return token;
+    }
+
+    private void revokeAllVerificationToken(User user) {
+        var verificationTokens = verificationTokenRepository.findByUser(user);
+        if (verificationTokens.isEmpty())
+            return;
+        verificationTokenRepository.deleteAll(verificationTokens);
+    }
+
+    public void verifyAccount(String token) {
+        Optional<VerificationToken> verificationToken = verificationTokenRepository.findByToken(token);
+        fetchUserAndEnable(verificationToken.orElseThrow(() -> new UserNotFoundException("Invalid Token")));
+    }
+
+    private void fetchUserAndEnable(VerificationToken verificationToken) {
+        String username = verificationToken.getUser().getUsername();
+        User  user = userRepository.findByEmail(username)
+                .orElseThrow(()-> new UserNotFoundException("User not found with name -" + username));
+        user.setEnabled(true);
+        revokeAllVerificationToken(user);
+        userRepository.save(user);
     }
 
     public void forgetMyPaswToken(String email) {
@@ -217,5 +169,11 @@ public class AuthenticationService {
         } else {
             throw new UserNotFoundException("Token is expired");
         }
+    }
+
+    public User getCurrentUser() {
+        User user = userRepository.findByEmail(SecurityContextHolder.getContext().getAuthentication().getName())
+                .orElseThrow(() -> new UserNotFoundException("Current user is not found"));
+        return user;
     }
 }
